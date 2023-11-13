7fc0f9a16a0cd2d2a03714d9bf15c98e1aac8184
mr xue <falethe@gmail.com>
2023-11-13 10:40:15 +0800

    init

--- app/controller/Upload.php
+++ app/controller/Upload.php
@@ -3,56 +3,44 @@
 namespace app\controller;
 
 use app\BaseController;
-use think\Request;
-
-// Define DS if not already defined
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
+use think\Facade\Request;
 
 class Upload extends BaseController
 {
     public function index()
     {
-        // 获取上传的文件
         $files = request()->file('file');
 
-        // 检查是否有文件上传
-        if (empty($files)) {
-            return json([
-                "code" => 400,
-                "msg" => "请选择文件",
-                "data" => null,
-            ]);
-        }
-
-        // 获取当前脚本所在的目录
-        $currentDir = __DIR__;
-        
-        // 指定文件保存的目录
-        $uploadDir = $currentDir . DS . '..' . DS . '..' . DS . 'public' . DS . 'uploads';
-
-        // 保存文件并获取保存的文件名
-        $fileNames = [];
         foreach ($files as $file) {
-            // 使用原始文件名保存文件
-            $info = $file->move($uploadDir, true);
-        
-            if ($info) {
-                $fileNames[] = $info->getSaveName();
-            } else {
-                return json([
-                    "code" => 500,
-                    "msg" => $file->getError() ?: '文件上传失败',
-                    "data" => null,
-                ]);
-            }
-        }
+            $ext = $file->getOriginalExtension();
+            //不同文件，储存不同的文件夹
+            $folder = config('filesystem.disks.folder') . '/uploads/' . $ext; //以文件后缀名作为存文件的存放目录
+            if (!file_exists($folder))
+                mkdir($folder, 0700, TRUE); //如果文件夹不存在，则创建
 
-        return json([
-            "code" => 200,
-            "msg" => "文件上传成功",
-            "data" => $fileNames,
-        ]);
+
+            $savename = \think\facade\Filesystem::disk('public')
+                ->putFile('', $file, 'md5'); //上传文件，得到上传之后的文件名称
+
+            if (!$savename) {
+                return json([
+                    "code"=>400,
+                    "msg"=>"文件上传失败",
+                    "data"=>null
+                ]);
+            } else {
+                $savename = '' . str_replace("\\", "/", $savename);
+                if ($savename) {
+                    return json([
+                        "code" => 200,
+                        "msg" => "文件上传成功",
+                        "data" => Request::domain() . '/uploads/' . $savename,
+                        //因为要返回给前端网址，这里要加上域名 Request::domain()
+                    ]);
+                } 
+            }
+
+        }
+      
     }
 }
